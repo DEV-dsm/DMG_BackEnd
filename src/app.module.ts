d59fc658ac17496ead5018cd7fa349fb942d53f4
XII <ur12xii@gmail.com>
2023-06-28 13:30:22 +0900

    💡 :: setting :: Added AppModule DB setting
    AppModule 파일에 Redis, TypeORM ( MySQL ) 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisModule } from '@liaoliaots/nestjs-redis'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true, // 캐싱
+      isGlobal: true, // 전역
+    }),
+    TypeOrmModule.forRoot({ // TypeORM
+      type: 'mysql',
+      host: process.env.DB_HOST, // 로컬 접속 호스트
+      port: 3306, // 포트
+      username: process.env.DB_USERNAME, // DB 접속 계정의 이름
+      password: process.env.DB_PASSWORD, // DB 접속 계정의 비밀번호
+      database: process.env.DB_NAME, // DB 테이블 이름
+      entities: [ __dirname + '/**/entity/*.js'],
+      synchronize: false, // false로 설정 안 하면 실행할 때마다 DB 날라감
+      logging: false, // 로그찍기
+      migrations: [__dirname + '/**/migrations/*.js'],
+      migrationsTableName: 'migrations',
+      autoLoadEntities: true,
+    }),
+    RedisModule.forRoot({ // 레디스
+      readyLog: true,
+      config: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        password: process.env.REDIS_PW,
+      }
+    }),
+  ],
   controllers: [],
   providers: [],
 })
