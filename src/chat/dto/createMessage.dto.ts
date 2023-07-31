a6dc8da617123ad1589f2fd437ba8251830010d3
XII <ur12xii@gmail.com>
2023-07-31 22:40:18 +0900

    👀 :: chore :: add class-validator for dto files
    - add the class validator for dto files

--- src/chat/dto/createMessage.dto.ts
+++ src/chat/dto/createMessage.dto.ts
@@ -1,12 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 export class CreateMessageDto {
+    @IsNumber()
     @ApiProperty({
         example: 1,
         description: "채팅을 보내는 그룹의 아이디"
     })
     groupID: number;
 
+    @IsString()
     @ApiProperty({
         example: "안녕하세요 DMG 개발팀입니다.",
         description: "채팅 내용"
