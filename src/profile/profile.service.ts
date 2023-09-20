97f3654ee7986b7cdb7bcc03bae162cdfc9c181b
12xii <ur12xii@gmail.com>
2023-09-20 14:29:43 +0900

    👀 :: chore :: deleted annotation
    - deleted annotation

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -49,22 +49,6 @@ export class ProfileService {
     async getStudentProfileList(accesstoken: string): Promise<object>{
         const { userID } = await this.userService.validateAccess(accesstoken);
 
-        // const userList = await this.userEntity.find({
-        //     where: { isStudent: true },
-        //     select: ['userID', 'name', 'profile' ]
-        // })
-
-        // let studentList = []
-
-        // for (let i = 0; i < userList.length; i++){
-        //     const student = await this.studentEntity.findOne({
-        //         where: { userID: userList[i].userID },
-        //         select: ['number']
-        //     })
-
-        //     studentList.push(Object.assign(userList[i], student));
-        // }
-
         const studentList = await this.userEntity
             .createQueryBuilder('qb')
             .innerJoin("qb.student", "student")
